2d492743b74cb78a71be75fa1a7103b6d36cc9eb
AVWORLD\rob9575 <rsantos@esri.com>
2018-03-23 14:42:50 -0500

    Got rid of uneeded imports in programmatic example

--- app/views/ProgrammaticValidationForm.js
+++ app/views/ProgrammaticValidationForm.js
@@ -1,17 +1,12 @@
 define([
     'dojo/_base/declare',
     'dijit/form/Form',
-    'dijit/_WidgetsInTemplateMixin',
-    //for whatever reason, this is looking in this directory, and not from the server root
-    'dojo/text!./ValidationForm.html',
     'dojox/layout/TableContainer', 
     'dijit/form/ValidationTextBox',
     'dojox/validate/web'
 ], function(
     declare,
     Form, 
-    _WidgetsInTemplateMixin, 
-    template, 
     TableContainer,
     ValidationTextBox
 ) {
